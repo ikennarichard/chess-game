358066a1f2636a7902a156f678a82824af362f7c
Ikenna Richard <oguejioforrichard@gmail.com>
2025-10-31 19:17:28 +0100

    feat: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import BoardPage from "./components/BoardPage";
+
+function App() {
+  return (
+    <div style={{ display: "grid", placeContent: "center" }}>
+      <BoardPage />
+    </div>
+  );
+}
+
+export default App;
